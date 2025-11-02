2eabdfd233bb66dcfe0984a2a915906debb8287a
DESKTOP-RAHNNQ0\A <63176347@qq.com>
2025-11-02 12:47:45 +0800

    我的ai博客站

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+  base: '/ai-blog/', // 👈 这里写你仓库的名字，前后要有斜杠
+  title: 'AI Blog',
+  description: '我的AI博客站'
+})
